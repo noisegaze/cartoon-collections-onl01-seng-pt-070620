cc4983d503877a14c281c544a7c9b2b0a063424d
literal-bookmark-0123 <ben.stephanus@gmail.com>
2020-07-16 15:41:34 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,8 +11,9 @@ def summon_captain_planet(array)
   array
 end
 
-def long_planeteer_calls
-  # Your code here
+def long_planeteer_calls(array)
+
+
 end
 
 def find_the_cheese# code an argument here
