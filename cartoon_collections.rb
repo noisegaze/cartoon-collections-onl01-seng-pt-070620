1409e466774287039d422f790e0f45b54ef96ef1
literal-bookmark-0123 <ben.stephanus@gmail.com>
2020-07-16 15:37:51 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -6,9 +6,9 @@ def roll_call_dwarves(array)
 end 
 def summon_captain_planet(array)
   array.map do |element|
-    puts "#{element}!".capitalize
+    puts "#{element}!"
   end
-  array
+  array.capitalize 
 end
 
 def long_planeteer_calls# code an argument here
