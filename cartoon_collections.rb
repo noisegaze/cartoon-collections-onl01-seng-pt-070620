c556caaf8ed96aa42ce528204b5908aaa873f1df
literal-bookmark-0123 <ben.stephanus@gmail.com>
2020-07-16 16:08:04 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -6,7 +6,7 @@ def roll_call_dwarves(array)
 end 
 def summon_captain_planet(array)
   array.map do |element|
-    puts "#{element}!".capitalize 
+    puts "#{element.capitalize}!"
   end
   array
 end
