6c0a6d6b967d7ca6c39e4a06c39d3cfcd98836ca
literal-bookmark-0123 <ben.stephanus@gmail.com>
2020-07-16 16:40:46 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -23,6 +23,7 @@ end
 def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
   array.find do |x|
+    binding.pry 
     x == "cheddar"|| x == "gouda" || x == "camembert"
   end
   
